4b389b638eaa9b9d3929fc36d4996e6a3b786aa9
s.Zhauken <47129007+s-awaken@users.noreply.github.com>
2022-04-04 12:42:46 +0600

    Update

--- src/app.constant.ts
+++ src/app.constant.ts
@@ -1,6 +1,10 @@
+import { genSalt } from 'bcrypt';
+
 export const AUTH_SERVICE = 'AUTH_SERVICE';
 export const USER_SERVICE = 'USER_SERVICE';
 
 export const jwtConstants = {
   secret: 'secretKey',
 };
+export const rounds = 10;
+export const salt = genSalt(rounds);
